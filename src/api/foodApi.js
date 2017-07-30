be3723f1fe875c81f944cf873812f826f9b66ae2
antoanelenkov <antoanelenkov@gmail.com>
2017-07-30 18:09:32 +0300

    foodApi using promises and imitate remote service calls

--- src/api/foodApi.js
+++ src/api/foodApi.js
@@ -1,40 +1,60 @@
-import _foods from './foods.json'
+import _foods from './foods.js'
 
-const foodApi = (function(){
-    const api={};
+const foodApi = (function () {
+    const api = {};
 
     const foods = _foods;
+    const remoteCallDelay = 500;
 
     let idGenerator = _foods.length;
 
-    api.getAll = function(){
-        return foods;
+    api.getAll = function () {
+        return new Promise(function (resolve, reject) {
+            setTimeout(() => { resolve(foods) }, remoteCallDelay)
+        });
     };
 
-    api.add = function(name,type,calories,protein,carbs,fats){
-        foods[++idGenerator]={
-            name,
-            type,
-            calories,
-            protein,
-            carbs,
-            fats
-        };
+    api.add = function (name, type, calories, protein, carbs, fats) {
+        return new Promise(function (resolve, reject) {
+            setTimeout(() => {
+                foods[++idGenerator] = {
+                    name,
+                    type,
+                    calories,
+                    protein,
+                    carbs,
+                    fats
+                };
+
+                resolve(foods[idGenerator]);
+            }, remoteCallDelay)
+        });
     };
 
-    api.update = function(id,name,type,calories,protein,carbs,fats){
-        foods[id] ={
-            name,
-            type,
-            calories,
-            protein,
-            carbs,
-            fats
-        };
+    api.update = function (id, name, type, calories, protein, carbs, fats) {
+        return new Promise(function (resolve, reject) {
+            setTimeout(() => {
+                foods[id] = {
+                    name,
+                    type,
+                    calories,
+                    protein,
+                    carbs,
+                    fats
+                };
+
+                resolve(foods[id]);
+            }, remoteCallDelay)
+        });
     };
 
-    api.delete = function(id){
-        delete foods[id];
+    api.delete = function (id) {
+        return new Promise(function (resolve, reject) {
+            setTimeout(() => {
+                delete foods[id];
+                resolve();
+            }, remoteCallDelay)
+        });
     }
 
     return api;

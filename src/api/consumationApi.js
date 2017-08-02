3cc8dd60c7a9df519046eeb9a7438f8e702761fb
antoanelenkov <antoanelenkov@gmail.com>
2017-08-02 14:35:19 +0300

    added info for proteins, carbs, fats and calories in consumation page

--- src/api/consumationApi.js
+++ src/api/consumationApi.js
@@ -1,4 +1,5 @@
 import dateFormatter from '../utils/dateFormatter';
+import * as CaloriesCountType from '../constants/CaloriesCountType';
 
 export default (function () {
     const api = {};
@@ -36,6 +37,7 @@ export default (function () {
         function _increaseQuantity(consum){
             const consumToUpdate = Object.assign({},consum);
             consumToUpdate.quantity += quantity*1;
+            _calculateMacronutrients(consumToUpdate);
             consumation[consumToUpdate.id-1] = consumToUpdate;
 
             return consumToUpdate;
@@ -49,9 +51,33 @@ export default (function () {
                 quantity: quantity*1
             };
 
+            _calculateMacronutrients(itemToSave);
+
             id ? (consumation[id-1]=itemToSave) : consumation.push(itemToSave);
             
             return itemToSave;
+        }
+
+        function _calculateMacronutrients(consumation){
+            if(consumation.food.type === CaloriesCountType.PER_HUNDRED_GRAMS){
+                Object.assign(consumation, {
+                    proteins:consumation.food.proteins*consumation.quantity/100,
+                    carbs:consumation.food.carbs*consumation.quantity/100,
+                    fats:consumation.food.fats*consumation.quantity/100,
+                    calories:consumation.food.calories*consumation.quantity/100
+                })
+            }
+            else if(consumation.food.type === CaloriesCountType.PER_ITEM){
+                Object.assign(consumation,{
+                    proteins:consumation.food.proteins*consumation.quantity,
+                    carbs:consumation.food.carbs*consumation.quantity,
+                    fats:consumation.food.fats*consumation.quantity,
+                    calories: consumation.food.calories*consumation.quantity
+                })                  
+            }
+            else{
+                throw 'unsupported type';
+            }
         }
     };
 
--- src/components/consumation/ConsumationPageView.js
+++ src/components/consumation/ConsumationPageView.js
@@ -14,6 +14,10 @@ export default (props) => {
                                     <tr>
                                         <th>Food</th>
                                         <th>Quantity</th>
+                                        <th>Proteins</th>
+                                        <th>Carbs</th>
+                                        <th>Fats</th>
+                                        <th>Calories</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -22,6 +26,10 @@ export default (props) => {
                                             <tr key={consumation.id}>
                                                 <td>{consumation.food.name}</td>
                                                 <td>{consumation.quantity}</td>
+                                                <td>{consumation.proteins}</td>
+                                                <td>{consumation.carbs}</td>
+                                                <td>{consumation.fats}</td>
+                                                <td>{consumation.calories}</td>
                                                 <td><input type="button" className="btn btn-primary" value="Edit" data-consumation-id={consumation.id} onClick={props.handleOnClick} /></td>
                                                 <td><input type="button" className="btn btn-primary" value="Delete" data-consumation-id={consumation.id} onClick={props.handleOnClick} /></td>
                                             </tr>
